3fb7ce680646dfe4dfef9388a118f5bcf6dabe29
Gideon Stek <70816440+gideon-stek@users.noreply.github.com>
2020-09-06 18:14:59 +0200

    langzamere dots

--- main.ts
+++ main.ts
@@ -17,10 +17,10 @@ basic.forever(function () {
     if (Richting == 1) {
         Random_nummer = randint(0, 4)
         Noord_nummer = game.createSprite(Random_nummer, 0)
-        basic.pause(350)
+        basic.pause(400)
         Noord_nummer.change(LedSpriteProperty.Y, 1)
         while (Noord_nummer.get(LedSpriteProperty.Y) < 4) {
-            basic.pause(350)
+            basic.pause(400)
             Noord_nummer.change(LedSpriteProperty.Y, 1)
             if (Noord_nummer.isTouching(sprite)) {
                 music.playTone(147, music.beat(BeatFraction.Double))
@@ -28,7 +28,7 @@ basic.forever(function () {
                 game.gameOver()
             }
         }
-        basic.pause(350)
+        basic.pause(400)
         if (Tijd != 350) {
             Tijd += -50
         }
@@ -68,10 +68,10 @@ basic.forever(function () {
     if (Richting == 4) {
         Random_nummer = randint(0, 4)
         West_nummer = game.createSprite(0, Random_nummer)
-        basic.pause(350)
+        basic.pause(400)
         West_nummer.change(LedSpriteProperty.X, 1)
         while (West_nummer.get(LedSpriteProperty.X) < 4) {
-            basic.pause(350)
+            basic.pause(400)
             West_nummer.change(LedSpriteProperty.X, 1)
             if (West_nummer.isTouching(sprite)) {
                 music.playTone(147, music.beat(BeatFraction.Double))
@@ -79,7 +79,7 @@ basic.forever(function () {
                 game.gameOver()
             }
         }
-        basic.pause(350)
+        basic.pause(400)
         if (Tijd != 350) {
             Tijd += -50
         }
@@ -91,10 +91,10 @@ basic.forever(function () {
     if (Richting == 2) {
         Random_nummer = randint(0, 4)
         Oost_nummer = game.createSprite(4, Random_nummer)
-        basic.pause(350)
+        basic.pause(400)
         Oost_nummer.change(LedSpriteProperty.X, -1)
         while (Oost_nummer.get(LedSpriteProperty.X) > 0) {
-            basic.pause(350)
+            basic.pause(400)
             Oost_nummer.change(LedSpriteProperty.X, -1)
             if (Oost_nummer.isTouching(sprite)) {
                 music.playTone(147, music.beat(BeatFraction.Double))
@@ -102,7 +102,7 @@ basic.forever(function () {
                 game.gameOver()
             }
         }
-        basic.pause(350)
+        basic.pause(400)
         if (Tijd != 350) {
             Tijd += -50
         }
@@ -114,10 +114,10 @@ basic.forever(function () {
     if (Richting == 3) {
         Random_nummer = randint(0, 4)
         Zuid_nummer = game.createSprite(Random_nummer, 4)
-        basic.pause(350)
+        basic.pause(400)
         Zuid_nummer.change(LedSpriteProperty.Y, -1)
         while (Zuid_nummer.get(LedSpriteProperty.Y) > 0) {
-            basic.pause(350)
+            basic.pause(400)
             Zuid_nummer.change(LedSpriteProperty.Y, -1)
             if (Zuid_nummer.isTouching(sprite)) {
                 music.playTone(147, music.beat(BeatFraction.Double))
@@ -125,7 +125,7 @@ basic.forever(function () {
                 game.gameOver()
             }
         }
-        basic.pause(350)
+        basic.pause(400)
         if (Tijd != 350) {
             Tijd += -50
         }
