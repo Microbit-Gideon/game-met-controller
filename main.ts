4c845257dc0fd9e1361a9501a8f4cec4777a73d3
Gideon Stek <70816440+gideon-stek@users.noreply.github.com>
2020-09-06 16:19:13 +0200

    Vier richtingen dots

--- main.ts
+++ main.ts
@@ -1,20 +1,55 @@
 WSJoyStick.onKey(KEY.F, function () {
     Random_nummer = randint(0, 4)
-    Noord_nummer = game.createSprite(0, Random_nummer)
+    West_nummer = game.createSprite(0, Random_nummer)
+    basic.pause(400)
+    West_nummer.change(LedSpriteProperty.X, 1)
+    while (West_nummer.get(LedSpriteProperty.X) < 4) {
+        basic.pause(400)
+        West_nummer.change(LedSpriteProperty.X, 1)
+    }
+    basic.pause(400)
+    West_nummer.delete()
 })
 WSJoyStick.onKey(KEY.E, function () {
     Random_nummer = randint(0, 4)
     Noord_nummer = game.createSprite(Random_nummer, 0)
+    basic.pause(400)
+    Noord_nummer.change(LedSpriteProperty.Y, 1)
+    while (Noord_nummer.get(LedSpriteProperty.Y) < 4) {
+        basic.pause(400)
+        Noord_nummer.change(LedSpriteProperty.Y, 1)
+    }
+    basic.pause(400)
+    Noord_nummer.delete()
 })
 WSJoyStick.onKey(KEY.D, function () {
     Random_nummer = randint(0, 4)
-    Noord_nummer = game.createSprite(4, Random_nummer)
+    Oost_nummer = game.createSprite(4, Random_nummer)
+    basic.pause(400)
+    Oost_nummer.change(LedSpriteProperty.X, -1)
+    while (Oost_nummer.get(LedSpriteProperty.X) > 0) {
+        basic.pause(400)
+        Oost_nummer.change(LedSpriteProperty.X, -1)
+    }
+    basic.pause(400)
+    Oost_nummer.delete()
 })
 WSJoyStick.onKey(KEY.C, function () {
     Random_nummer = randint(0, 4)
-    Noord_nummer = game.createSprite(Random_nummer, 4)
+    Zuid_nummer = game.createSprite(Random_nummer, 4)
+    basic.pause(400)
+    Zuid_nummer.change(LedSpriteProperty.Y, -1)
+    while (Zuid_nummer.get(LedSpriteProperty.Y) > 0) {
+        basic.pause(400)
+        Zuid_nummer.change(LedSpriteProperty.Y, -1)
+    }
+    basic.pause(400)
+    Zuid_nummer.delete()
 })
+let Zuid_nummer: game.LedSprite = null
+let Oost_nummer: game.LedSprite = null
 let Noord_nummer: game.LedSprite = null
+let West_nummer: game.LedSprite = null
 let Random_nummer = 0
 WSJoyStick.JoyStickInit()
 let sprite = game.createSprite(2, 2)
