7e83e61c6e3110ea76fc5e8ac0c6311606e2d65f
Jonathan Stek <70843066+Jona686@users.noreply.github.com>
2020-09-06 17:38:14 +0200

    Merge changes Jonathan

--- main.ts
+++ main.ts
@@ -1,95 +1,79 @@
 WSJoyStick.onKey(KEY.F, function () {
-    Random_nummer = randint(0, 4)
-    West_nummer = 0
-    for (let index = 0; index < 5; index++) {
-        if (Random_nummer != West_nummer) {
-            Muur_west = game.createSprite(0, West_nummer)
-        }
-        West_nummer += 1
-    }
-})
-WSJoyStick.onKey(KEY.P, function () {
-    Muur = game.createSprite(0, 0)
-    for (let index = 0; index < 4; index++) {
-        basic.pause(1000)
-        Muur.change(LedSpriteProperty.Y, 1)
-    }
+    Player.change(LedSpriteProperty.X, -1)
 })
 WSJoyStick.onKey(KEY.E, function () {
-    Random_nummer = randint(0, 4)
-    Noord_nummer = 0
-    for (let index = 0; index < 5; index++) {
-        if (Random_nummer != Noord_nummer) {
-            Muur_noord = game.createSprite(Noord_nummer, 0)
-        }
-        Noord_nummer += 1
-    }
-    Noord_nummer = 0
-    for (let index = 0; index < 4; index++) {
-        basic.pause(450)
-    }
+    Player.change(LedSpriteProperty.Y, 1)
 })
 WSJoyStick.onKey(KEY.D, function () {
-    Random_nummer = randint(0, 4)
-    Oost_nummer = 0
-    for (let index = 0; index < 5; index++) {
-        if (Random_nummer != Oost_nummer) {
-            Muur_oost = game.createSprite(4, Oost_nummer)
-        }
-        Oost_nummer += 1
-    }
+    Player.change(LedSpriteProperty.X, 1)
 })
 WSJoyStick.onKey(KEY.C, function () {
-    Random_nummer = randint(0, 4)
-    Zuid_nummer = 0
-    for (let index = 0; index < 5; index++) {
-        if (Random_nummer != Zuid_nummer) {
-            Muur_zuid = game.createSprite(Zuid_nummer, 4)
-        }
-        Zuid_nummer += 1
+    Player.change(LedSpriteProperty.Y, -1)
+})
+let list: game.LedSprite[] = []
+let Deleted_sprite = 0
+let Direction2 = 0
+let Sprite_5: game.LedSprite = null
+let Sprite_4: game.LedSprite = null
+let Sprite_3: game.LedSprite = null
+let Sprite_2: game.LedSprite = null
+let Sprite_1: game.LedSprite = null
+let Player: game.LedSprite = null
+Player = game.createSprite(2, 2)
+basic.forever(function () {
+    if (Player.isTouching(Sprite_1) || (Player.isTouching(Sprite_2) || Player.isTouching(Sprite_3) || (Player.isTouching(Sprite_4) || Player.isTouching(Sprite_5)))) {
+        game.gameOver()
     }
 })
-let Muur_zuid: game.LedSprite = null
-let Zuid_nummer = 0
-let Muur_oost: game.LedSprite = null
-let Oost_nummer = 0
-let Muur_noord: game.LedSprite = null
-let Noord_nummer = 0
-let Muur: game.LedSprite = null
-let Muur_west: game.LedSprite = null
-let West_nummer = 0
-let Random_nummer = 0
-WSJoyStick.JoyStickInit()
-let sprite = game.createSprite(2, 2)
-let _300 = 300
 basic.forever(function () {
-    if (WSJoyStick.Listen_Dir(DIR.U)) {
-        sprite.change(LedSpriteProperty.Y, -1)
-        basic.pause(_300)
-    } else if (WSJoyStick.Listen_Dir(DIR.D)) {
-        sprite.change(LedSpriteProperty.Y, 1)
-        basic.pause(_300)
-    } else if (WSJoyStick.Listen_Dir(DIR.L)) {
-        sprite.change(LedSpriteProperty.X, -1)
-        basic.pause(_300)
-    } else if (WSJoyStick.Listen_Dir(DIR.R)) {
-        sprite.change(LedSpriteProperty.X, 1)
-        basic.pause(_300)
-    } else if (WSJoyStick.Listen_Dir(DIR.U_L)) {
-        sprite.change(LedSpriteProperty.Y, -1)
-        sprite.change(LedSpriteProperty.X, -1)
-        basic.pause(_300)
-    } else if (WSJoyStick.Listen_Dir(DIR.U_R)) {
-        sprite.change(LedSpriteProperty.Y, -1)
-        sprite.change(LedSpriteProperty.X, 1)
-        basic.pause(_300)
-    } else if (WSJoyStick.Listen_Dir(DIR.D_L)) {
-        sprite.change(LedSpriteProperty.Y, 1)
-        sprite.change(LedSpriteProperty.X, -1)
-        basic.pause(_300)
-    } else if (WSJoyStick.Listen_Dir(DIR.D_R)) {
-        sprite.change(LedSpriteProperty.Y, 1)
-        sprite.change(LedSpriteProperty.X, 1)
-        basic.pause(_300)
+    while (game.isRunning()) {
+        Direction2 = randint(0, 3)
+        if (Direction2 == 0) {
+            Sprite_1 = game.createSprite(0, 4)
+            Sprite_2 = game.createSprite(1, 4)
+            Sprite_3 = game.createSprite(2, 4)
+            Sprite_4 = game.createSprite(3, 4)
+            Sprite_5 = game.createSprite(4, 4)
+        } else if (Direction2 == 1) {
+            Sprite_1 = game.createSprite(0, 0)
+            Sprite_2 = game.createSprite(0, 1)
+            Sprite_3 = game.createSprite(0, 2)
+            Sprite_4 = game.createSprite(0, 3)
+            Sprite_5 = game.createSprite(0, 4)
+        } else if (Direction2 == 2) {
+            Sprite_1 = game.createSprite(0, 0)
+            Sprite_2 = game.createSprite(1, 0)
+            Sprite_3 = game.createSprite(2, 0)
+            Sprite_4 = game.createSprite(3, 0)
+            Sprite_5 = game.createSprite(4, 0)
+        } else {
+            Sprite_1 = game.createSprite(4, 0)
+            Sprite_2 = game.createSprite(4, 1)
+            Sprite_3 = game.createSprite(4, 2)
+            Sprite_4 = game.createSprite(4, 3)
+            Sprite_5 = game.createSprite(4, 4)
+        }
+        Sprite_1.set(LedSpriteProperty.Direction, Direction2 * 90)
+        Sprite_2.set(LedSpriteProperty.Direction, Direction2 * 90)
+        Sprite_3.set(LedSpriteProperty.Direction, Direction2 * 90)
+        Sprite_4.set(LedSpriteProperty.Direction, Direction2 * 90)
+        Sprite_5.set(LedSpriteProperty.Direction, Direction2 * 90)
+        Deleted_sprite = randint(0, 4)
+        list = [Sprite_1, Sprite_2, Sprite_3, Sprite_4, Sprite_5]
+        list[Deleted_sprite].delete()
+        for (let index = 0; index < 4; index++) {
+            basic.pause(1000)
+            Sprite_1.move(1)
+            Sprite_2.move(1)
+            Sprite_3.move(1)
+            Sprite_4.move(1)
+            Sprite_5.move(1)
+        }
+        basic.pause(1000)
+        Sprite_1.delete()
+        Sprite_2.delete()
+        Sprite_3.delete()
+        Sprite_4.delete()
+        Sprite_5.delete()
     }
 })
